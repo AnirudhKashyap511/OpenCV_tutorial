e1d7e87b026bbbd4ffa2455221c29a6630d22c17
Anirudh Ameya Kashyap <anirudhameya@gmail.com>
2019-08-10 02:16:32 +0530

    Moving images to the image folder

--- opencv.py
+++ opencv.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib
 matplotlib.use('agg')
 import matplotlib.pyplot as plt 
-img = cv2.imread('tiger.jpg',cv2.IMREAD_GRAYSCALE)
+img = cv2.imread('/home/ani/opencv_tut/Images/tiger.jpg',cv2.IMREAD_GRAYSCALE)
 cv2.imshow('image',img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
